db7e13da0e0d71d0d7b6b47de2db3abf5da84f7b
Saif-Yehia-Mosaad <saifyehia58@gmail.com>
2025-11-24 00:29:34 +0200

    Application On Data Annotations & Application Validation

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -10,15 +10,16 @@ namespace Company.Models
     ///EF support 4 ways for mapping classes to database(Table, viwe, function)
     ///1.by convention(default behavior)
 
+///2.Data Annotations (Set Of Attributes)
+
     ///poco class
     ///plain old clr object (don't contain any functionality or behaviors )
 {
     internal class Employee
     {
-        // ? => Nullable
-        public int Id { get; set; } //public numeric property named as "Id" | "EmployeeId --> pk[Use Identity(1,1)]
-
-        //Add New Column
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //(1,1)
+        public int EmpId { get; set; }
         public string? Name { get; set; } //reference type :alow null[optional]
         public string Salary { get; set; } // value type : not allow null[required]
         public int? Age { get; set; } //Nullable<int> : alow null[optional]
--- Program.cs
+++ Program.cs
@@ -12,15 +12,6 @@ namespace Company
         {
          CompanyDbContext dbContext = new CompanyDbContext();
 
-            //dbContext.Employees.Where(E => E.Id == 1); Linq
-
-            //dbContext.Database.EnsureDeleted();//If Exist Delete It
-            //          // ===>This Way Isn't Effective Anymore Because In This Way Any Change Affect In Whole Data 
-            //dbContext.Database.EnsureCreated();//If Not Exist Create It
-            //But In
-            //Migration Use To Make Manipulating Whith Db Consistence And  Apply Changes Without Affect On Existing Data More Effective Than Enshure
-
-            //dbContext.Database.Migrate();//Apply All Migration That Not Applied Yet
 
 
         }
